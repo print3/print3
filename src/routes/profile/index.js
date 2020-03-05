1cd65f6c91a02ef74723231d4f48c7ef5434866d
afflitto <andrew@afflitto.tech>
2020-03-05 01:12:13 -0500

    show user info on the profile route

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,48 +1,48 @@
 import { h, Component } from 'preact';
 import Button from 'preact-material-components/Button';
+import LinearProgress from 'preact-material-components/LinearProgress';
 import 'preact-material-components/Button/style.css';
+import 'preact-material-components/LinearProgress/style.css';
 import style from './style';
+
+import { github } from '../../github.js';
 
 export default class Profile extends Component {
 	state = {
-		time: Date.now(),
-		count: 10
+		loading: true,
+		user: null,
 	};
 
-	// gets called when this route is navigated to
-	componentDidMount() {
-		// start a timer for the clock:
-		this.timer = setInterval(this.updateTime, 1000);
+	constructor() {
+		super();
+
+		this.gh = github;
+		this.fetchUser();
 	}
 
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		clearInterval(this.timer);
+	async fetchUser() {
+		const user = await this.gh.users.getAuthenticated();
+
+		this.setState({
+			user: user,
+			loading: false,
+		});
 	}
 
-	// update the current time
-	updateTime = () => {
-		this.setState({ time: Date.now() });
-	};
+	render() {
+		if(this.state.loading) {
+			return (
+				<div class={style.loader}>
+					<LinearProgress indeterminate />
+				</div>
+			);
+		}
 
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-	};
-
-	// Note: `user` comes from the URL, courtesy of our router
-	render({ user }, { time, count }) {
 		return (
 			<div class={`${style.profile} page`}>
-				<h1>Profile: {user}</h1>
-				<p>This is the user profile for a user named { user }.</p>
-
-				<div>Current time: {new Date(time).toLocaleString()}</div>
-
-				<p>
-					<Button raised ripple onClick={this.increment}>Click Me</Button>
-					{' '}
-					Clicked {count} times.
-				</p>
+				<h1>Profile: {this.state.user.data.login}</h1>
+				<p>This is the user profile for a user named { this.state.user.data.name }.</p>
+				<p>coming soon</p>
 			</div>
 		);
 	}
